49513b9b5808e25f476018fb8e205c358540ccc8
vineeta singh <vineeta.abhi82@gmail.com>
2026-01-30 08:21:43 +0100

    Update home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins- Koi to chala do web-hook!!"
+    return "Hello from Jenkins- Webhook is working!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
